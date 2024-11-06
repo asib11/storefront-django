5735d303b63190cad4d8010d8c2025baeb39d395
asib <ahmedasib1@gmail.com>
2024-11-06 12:01:37 +0600

    reverting orm

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,14 @@ from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
+    # query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+    
+    #first element gets none
+    exists = Product.objects.filter(pk=0).first()
+
+    #get boolean
+    exists = Product.objects.filter(pk=0).exists()
+
     return render(request, 'hello.html', {'name': 'Asib'})
