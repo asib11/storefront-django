84aa0fd7a90327bbaa7c5c1ac0356fc149837ed0
Asib <ahmedasib1@gmail.com>
2025-04-04 12:58:12 +0600

    feat #1 : added computed column

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,15 @@ from . import models
 # Register your models here.
 @admin.register(models.Product) # decorator is the best way to register models
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory', 'collection']
-    list_editable = ['unit_price', 'inventory']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
