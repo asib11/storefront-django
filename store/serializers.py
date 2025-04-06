c91daa00bcdcfef9f6f20a8ab85c9e07e68098b2
Asib <ahmedasib1@gmail.com>
2025-04-06 19:12:38 +0600

    feat #2 :use ModelSerializer instead fo serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,25 +2,42 @@ from rest_framework import serializers
 from decimal import Decimal
 from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+# model serializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(method_name='claulate_tax') #custom sericalizer method
     
-
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='claulate_tax') #custom sericalizer method field
-    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all(), source='collection.id')
-    # collection = serializers.StringRelatedField(queryset=Collection.objects.all())
-    collection = CollectionSerializer() # nested serializer
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-        # lookup_field='id'
-    )
-
     def claulate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+  
+
+#normal serializer
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+#     price_with_tax = serializers.SerializerMethodField(method_name='claulate_tax') #custom sericalizer method field
+#     # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all(), source='collection.id')
+#     # collection = serializers.StringRelatedField(queryset=Collection.objects.all())
+#     collection = CollectionSerializer() # nested serializer
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset=Collection.objects.all(),
+#         view_name='collection-detail',
+
+#     )
+
+#     def claulate_tax(self, product: Product):
+#         return product.unit_price * Decimal(1.1)
--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('product/', views.product_list),
     path('product/<int:id>/', views.product_detail),
     # path('collection/', views.collection_list),
-    path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
+    path('collection/<int:pk>/', views.collection_detail, name='collection-detail'), # strandard naming convention is pk instad of id
 ]
