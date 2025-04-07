451f9fa13c20c37dc50cb45d6e8d2e4ea40a57ab
Asib <ahmedasib1@gmail.com>
2025-04-08 01:34:49 +0600

    feat #3 : class view convert

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from django.db.models import Count
+from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 class ProductList(APIView):
@@ -19,18 +21,6 @@ class ProductList(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# function based view
-# @api_view(['GET', 'POST'])
-# def product_list(request):
-#     if request.method == 'GET':
-#         queryset = Product.objects.select_related('collection').all()
-#         serializer = ProductSerializer(queryset, many=True, context={'request': request})
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = ProductSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ProductDetail(APIView):
     def get(self, request, id):
@@ -52,35 +42,35 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# function based view
-#shortcut and rest of the code we will write this format
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def product_detail(request, id):
-#     product = get_object_or_404(Product,pk=id)
-#     if request.method == 'GET':
-#         serializer = ProductSerializer(product)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = ProductSerializer(product, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-#     elif request.method == 'DELETE':
-#         if product.order_items.count() > 0:
-#             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-#         product.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class CollectionList(APIView):
+    def get(self, request):
+        queryset = Collection.objects.annotate(products_count=Count('product')).all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# @api_view()
-# def product_detail(request, id):
-#     try:
-#         product = Product.objects.get(pk=id)
-#         serializer = ProductSerializer(product)
-#         return Response(serializer.data)
-#     except Product.DoesNotExist:
-#         return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class CollectionDetails(APIView):
     def get(self, request, pk):
-        return Response('OK')
+        collection = get_object_or_404(Collection, pk=pk)
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        if collection.product.count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it has products associated with it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
