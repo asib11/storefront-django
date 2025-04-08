1e3bd494ae14ebd853e6305f1e804f4816f5ef06
Asib <ahmedasib1@gmail.com>
2025-04-08 23:56:05 +0600

    feat #3 : genetic filtering

--- store/views.py
+++ store/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
+from store.filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
 from django.db.models import Count
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -12,14 +10,18 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 
 # viewsets
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['collection_id', 'unit_price']
+    filterset_class = ProductFilter
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset 
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     collection_id = self.request.query_params.get('collection_id')
+    #     if collection_id is not None:
+    #         queryset = queryset.filter(collection_id=collection_id)
+    #     return queryset 
 
     def get_serializer_context(self):
         return  {'request': self.request,}
