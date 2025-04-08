f771bd0f524d810f2822bb415bc76cf680554d1f
Asib <ahmedasib1@gmail.com>
2025-04-08 21:49:47 +0600

    fix #3 : nested review router make

--- store/serializers.py
+++ store/serializers.py
@@ -51,7 +51,11 @@ class ProductSerializer(serializers.ModelSerializer):
 #     def claulate_tax(self, product: Product):
 #         return product.unit_price * Decimal(1.1)
 
-class ReviewSerializer(serializers.Serializer):
+class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'name', 'description', 'date', 'product']
+        fields = ['id', 'date','name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id = product_id, **validated_data)
--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-router = DefaultRouter()
-router.register('products', views.ProductViewSet, basename='product')
-router.register('collections', views.CollectionViewSet, basename='collection')
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+# Nested router
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+product_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + product_router.urls
 
 # # URLConf
 # urlpatterns = [
