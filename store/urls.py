884901fcb41fb517d48505715b9d89c04111845e
Asib <ahmedasib1@gmail.com>
2025-04-08 18:32:26 +0600

    fix #3 : delete button fix and use router

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet, basename='product')
+router.register('collections', views.CollectionViewSet, basename='collection')
 
-# URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()), # class based view
-    path('products/<int:pk>/', views.ProductDetail.as_view()), # class based view
-    path('collections/', views.CollectionList.as_view()), # class based view
-    path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-detail'), # strandard naming convention is pk instad of id
-]
+urlpatterns = router.urls
+
+# # URLConf
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()), # class based view
+#     path('products/<int:pk>/', views.ProductDetail.as_view()), # class based view
+#     path('collections/', views.CollectionList.as_view()), # class based view
+#     path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-detail'), # strandard naming convention is pk instad of id
+# ]
--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection
+from .models import OrderItem, Product, Collection
 from django.db.models import Count
 from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
@@ -18,12 +18,11 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return  {'request': self.request,}
     
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.order_items.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
 
 # class ProductList(ListCreateAPIView):
 #     #generic view
@@ -56,12 +55,17 @@ class CollectionViewSet(ModelViewSet):
     def get_serializer_context(self):
         return  {'request': self.request,}
     
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.product.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it has products associated with it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+    
+    # def delete(self, request, pk):
+    #     collection = get_object_or_404(Collection, pk=pk)
+    #     if collection.product.count() > 0:
+    #         return Response({'error': 'Collection cannot be deleted because it has products associated with it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 # class CollectionList(ListCreateAPIView):
 #     #generic view
