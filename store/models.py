5ee5801d70bb8bd83e49c79c1fbf581c3ce14f6d
Asib <ahmedasib1@gmail.com>
2025-04-14 12:00:25 +0600

    feat #10 : getting product and calculate total price of all products

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
-from uuid import uuid4
 from django.db import models
 from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -94,7 +94,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4,)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
