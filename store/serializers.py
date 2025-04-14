dbc64fd9f0e28813af99aba1a53ff5a64ca4264e
Asib <ahmedasib1@gmail.com>
2025-04-14 13:35:36 +0600

    feat #10 : delete cart

--- store/serializers.py
+++ store/serializers.py
@@ -78,7 +78,7 @@ class CartItemSerializer(serializers.ModelSerializer):
     
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
-    items = CartItemSerializer(many=True)
+    items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField()
     def get_total_price(self, cart: Cart):
         return sum(item.quantity * item.product.unit_price for item in cart.items.all())
