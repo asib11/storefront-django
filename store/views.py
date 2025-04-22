18acd153038d6012d7c6fa67f84033159d36d7d5
Asib <ahmedasib1@gmail.com>
2025-04-22 14:20:04 +0600

    feat #20 : returning images from the API

--- store/views.py
+++ store/views.py
@@ -16,7 +16,7 @@ from .serializers import AddCartItemSericalizer, CartItemSerializer, CartSeriali
 
 # viewsets
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # filterset_fields = ['collection_id', 'unit_price']
