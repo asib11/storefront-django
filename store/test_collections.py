52e7375685dde2b77668b877f4c65e93c28663ce
Asib <ahmedasib1@gmail.com>
2025-04-28 20:08:19 +0600

    feat #26 : running first test

--- store/test_collections.py
+++ store/test_collections.py
@@ -1,8 +1,10 @@
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
+@pytest.mark.django_db
 class TestCreateCollections:
-    def test_if_user_is_anonymous(self, client):
+    def test_if_user_is_anonymous_retrun_401(self):
         client = APIClient()
         response = client.post('/store/collections/', {
             'title': 'a',
