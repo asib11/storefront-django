91de666f9f3287132437758572657e9f02f76540
Asib <ahmedasib1@gmail.com>
2025-04-30 16:28:35 +0600

    feat #26 : single and multiple assert

--- store/test_collections.py
+++ store/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
@@ -19,3 +20,22 @@ class TestCreateCollections:
             'title': 'a',
         })
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_return_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {
+            'title': '',
+        })
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_retrun_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {
+            'title': 'a',
+        })
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+        
