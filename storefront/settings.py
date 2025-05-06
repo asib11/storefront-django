7741655984d7a1c4a642955b5729521b4d355aa4
Asib <ahmedasib1@gmail.com>
2025-05-06 19:36:31 +0600

    feat #29 : verify optimization

--- storefront/settings.py
+++ storefront/settings.py
@@ -64,10 +64,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if DEBUG:
-    MIDDLEWARE += [
-        'silk.middleware.SilkyMiddleware',
-    ]
+# if DEBUG:
+#     MIDDLEWARE += [
+#         'silk.middleware.SilkyMiddleware',
+#     ]
 
 INTERNAL_IPS = [
     # ...
