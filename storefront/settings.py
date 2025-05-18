bdf909af7d6d8b039c8daee685e4122f4a97a34e
Asib <ahmedasib1@gmail.com>
2025-05-18 11:52:30 +0600

    feat #30 : cache config setup

--- storefront/settings.py
+++ storefront/settings.py
@@ -202,3 +202,13 @@ CELERY_BEAT_SCHEDULE = {
         'args': ('Hello from Django',)
     },
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/5",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
