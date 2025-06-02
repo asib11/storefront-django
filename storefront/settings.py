4d70e04fce512e3093aba700ece90658225ae481
Asib Ahmed <ahmedasib1@gmail.com>
2025-06-02 23:57:51 +0600

    feat #33 : logging

--- storefront/settings.py
+++ storefront/settings.py
@@ -216,3 +216,17 @@ CACHES = {
         }
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class' : 'logging.StreamHandler'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log'
+        }
+    }
+}
